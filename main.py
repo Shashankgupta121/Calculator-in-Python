4dca345f8a8f62088282b3e7caa6a177622a9c86
Shashankgupta121 <75942419+Shashankgupta121@users.noreply.github.com>
2021-10-03 14:58:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ FIRST = int(input("ENTER YOUR FIRST NUMBER HERE: "))
 SECOND = int(input("ENTER YOUR SECOND NUMBER HERE: "))
 TO_DO = (input("WHAT TO DO WITH THESE TWO NUMBERS: "))
 if TO_DO=="+":
-    print(FIRST + SECOND)
-elif TO_DO=="-":
-    print(FIRST - SECOND)
+    print("THE ANSWER IS: " + str(FIRST + SECOND))
+    print("CODE FINISED :)")
+elif TO_DO=="add":
+    print("THE ANSWER IS: " + str(FIRST + SECOND))
+    print("CODE FINISED :)")
+elif TO_DO == "-":
+    print("THE ANSWER IS: " + str(FIRST - SECOND))
+    print("CODE FINISED :)")
+elif TO_DO == "cut":
+    print("THE ANSWER IS: " + str(FIRST - SECOND))
+    print("CODE FINISED :)")
 elif TO_DO=="*":
-    print(FIRST * SECOND)
+    print("THE ANSWER IS: " + str(FIRST * SECOND))
+    print("CODE FINISED :)")
+elif TO_DO=="mul":
+    print("THE ANSWER IS: " + str(FIRST * SECOND))
+    print("CODE FINISED :)")
 elif TO_DO=="/":
-    print(FIRST / SECOND )
+    print("THE ANSWER IS: " + str(FIRST / SECOND ))
+    print("CODE FINISED :)")
+elif TO_DO=="divide":
+    print("THE ANSWER IS: " + str(FIRST / SECOND ))
+    print("CODE FINISED :)")
 else:
     print("SOMETHING IS WRONG")
